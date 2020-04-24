34d5f4ca1b63d19a08ff5f93aece6e09b8eb3d95
arjanlangit <heaven.arjan@gmail.com>
2020-04-24 15:06:21 +0800

    installed react-datepicker

--- frontend/src/pages/add-expenses/add-expenses.component.jsx
+++ frontend/src/pages/add-expenses/add-expenses.component.jsx
@@ -1,103 +1,134 @@
-import React from 'react';
+import React from "react";
 
 // styled components
-import { AddExpenseContainer } from './add-expenses.styles';
+import { AddExpenseContainer } from "./add-expenses.styles";
+import DatePicker from "react-datepicker";
 
 // components
-import Forms from '../../components/forms/forms.component';
-import FormInput from '../../components/form-input/form-input.component';
-import CalendarInput from '../../components/calendar-input/calendar-input.component';
+import Forms from "../../components/forms/forms.component";
+import FormInput from "../../components/form-input/form-input.component";
+import CalendarInput from "../../components/calendar-input/calendar-input.component";
+import "react-datepicker/dist/react-datepicker.css";
 
 class AddExpensesPage extends React.Component {
-	state = {
-		transactionName: '',
-		date: '',
-		account: '',
-		totalAmount: '',
-		category: '',
-		note: '',
+  state = {
+    transactionName: "",
+    date: new Date(),
+    account: "",
+    totalAmount: "",
+    category: "",
+    note: "",
   };
-  
-  handleSubmit = async event => {
+
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    const { transactionName, date, account, totalAmount, category, note } = this.state;
+    const {
+      transactionName,
+      date,
+      account,
+      totalAmount,
+      category,
+      note,
+    } = this.state;
 
     try {
-      this.setState({ transactionName: '', date: '', account: '', totalAmount: '', category: '', note: '' });
+      this.setState({
+        transactionName: "",
+        date: "",
+        account: "",
+        totalAmount: "",
+        category: "",
+        note: "",
+      });
     } catch (error) {
       console.log(error);
     }
   };
 
-  handleChange = event => {
+  handleChange = (event) => {
     const { value, name } = event.target;
 
     this.setState({ [name]: value });
   };
-  
-	render() {
-		const { transactionName, date, account, totalAmount, category, note } = this.state;
 
-		return (
-			<AddExpenseContainer>
-				<Forms title='Transactions' subheading='Adding new transaction'>
+  dateChangedHandler = (date) => {
+    this.setState({
+      date: date,
+    });
+  };
+
+  render() {
+    const {
+      transactionName,
+      date,
+      account,
+      totalAmount,
+      category,
+      note,
+    } = this.state;
+
+    return (
+      <AddExpenseContainer>
+        <Forms title="Transactions" subheading="Adding new transaction">
           <CalendarInput />
           <form onSubmit={this.handleSubmit}>
-  			    <FormInput 
-              name='transactionName'
-              type='text'
+            <FormInput
+              name="transactionName"
+              type="text"
               handleChange={this.handleChange}
               value={transactionName}
-              label='Transation Name'
+              label="Transation Name"
               required
             />
-  			    <FormInput 
-              name='date'
-              type='date'
+            <DatePicker
+              selected={this.state.date}
+              onChange={this.dateChangedHandler}
+            />
+            <FormInput
+              name="date"
+              type="date"
               handleChange={this.handleChange}
               value={date}
-              label='Date'
+              label="Date"
               required
             />
-  			    <FormInput 
-              name='account'
-              type='text'
+            <FormInput
+              name="account"
+              type="text"
               handleChange={this.handleChange}
               value={account}
-              label='Account'
+              label="Account"
               required
             />
-  			    <FormInput 
-              name='amount'
-              type='number'
+            <FormInput
+              name="amount"
+              type="number"
               handleChange={this.handleChange}
               value={totalAmount}
-              label='Total Amount'
+              label="Total Amount"
               required
             />
-  			    <FormInput 
-              name='category'
-              type='text'
+            <FormInput
+              name="category"
+              type="text"
               handleChange={this.handleChange}
               value={category}
-              label='Category'
+              label="Category"
               required
             />
-  			    <FormInput 
-              name='note'
-              type='textarea'
+            <FormInput
+              name="note"
+              type="textarea"
               handleChange={this.handleChange}
               value={note}
-              label='Note'
+              label="Note"
             />
-
           </form>
-
-				</Forms>
-			</AddExpenseContainer>
-		);
-	}
+        </Forms>
+      </AddExpenseContainer>
+    );
+  }
 }
 
 export default AddExpensesPage;
